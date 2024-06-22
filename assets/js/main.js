42fa72844a1e8237f1a1e820d17cd8ba485bdf5b
Aditya Sharma <80837330+adityasharma06@users.noreply.github.com>
2024-06-22 15:58:31 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-function scrollHeader() {
-    const header = document.getElementById('header')
-    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    if (this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
-}
-window.addEventListener('scroll', scrollHeader)
 
 /*=============== SWIPER POPULAR ===============*/
 var swiperPopular = new Swiper(".popular__container", {
@@ -67,7 +61,6 @@ function scrollActive() {
         }
     })
 }
-window.addEventListener('scroll', scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/
 function scrollUp() {
